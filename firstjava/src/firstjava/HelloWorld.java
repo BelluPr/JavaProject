62fc009321ecb91a7c9a0bc9afd71142d5e035a1
Prakash <Prakash@DESKTOP-0TNM7I6>
2021-03-01 19:38:29 +0530

    added two patteren files

--- firstjava/src/firstjava/HelloWorld.java
+++ firstjava/src/firstjava/HelloWorld.java
@@ -1,18 +1,42 @@
 package firstjava;
 
-public class HelloWorld {
-	
-	public static void main(String args[])
+public class HelloWorld 
+{
+	int i, j;
+
+	public void printStarPattern()
 	{
-		int i,j;
-		for(i=1;i<=5;i++)
+		
+		for (i = 1; i <= 5; i++) 
 		{
-			for(j=1;j<=i;j++)
+			for (j = 1; j <= i; j++) 
 			{
 				System.out.print('*');
-				
+
 			}
 			System.out.println();
 		}
+	}
+	public void printnumPattern() 
+	{
+
+		for (i = 1; i <= 5; i++) 
+		{
+			for (j = 1; j <= i; j++) 
+			{
+				System.out.print(i);
+
+			}
+			System.out.println();
+		}
+
+}
+	
+
+	public static void main(String args[]) {
+		HelloWorld obj= new HelloWorld();
+		obj.printnumPattern();
+		obj.printStarPattern();
+		
 	}
 }
